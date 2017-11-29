98b9885ebc735205a990d5343960e5081aea4557
youngclean <mufeng_606@hotmail.com>
2017-11-29 15:10:06 +0800

    第一次提交

---
+++ README.md
@@ -0,0 +1,18 @@
+## ystart
+简易脚手架，快速搭建一个不包含任何框架或库的项目，根据开发需求增加依赖包
+
+## 安装
+`npm install ystart -g || cnpm install ystart -g`
+
+## 使用
+在要开发代码的根目录下运行
+
+`ystart init`
+
+根据命令创建项目名称及htdocs目录
+
+`cd 项目目录 && npm install || cnpm install`
+
+下载完所有包后，启动本地开发环境
+
+`npm start`
---
+++ command/init.js
@@ -0,0 +1,57 @@
+"use strict";
+const exec = require("child_process").exec;
+const co = require("co");
+const prompt = require("co-prompt");
+const config = require("../config");
+const chalk = require("chalk");
+const shelljs = require("shelljs");
+const fs = require("fs");
+
+function execPromise(command) {
+  return new Promise((resolve, reject) => {
+    console.log("start to exec commands:");
+    console.log(command);
+    console.log("------------");
+    const cmd = exec(command, err => {
+      if (err) {
+        return reject(err);
+      }
+      resolve();
+    });
+
+    cmd.stdout.on("data", data => {
+      console.log(data);
+    });
+  });
+}
+
+module.exports = () => {
+  co(function*() {
+    // 处理用户输入
+    const projectName = yield prompt("Project name: ");
+    const gitUrl =
+      "https://github.com/youngclean/webpack-hot-demo.git";
+
+    // 避免重复添加
+    if (!config.project[projectName]) {
+      config.project[projectName] = {};
+      config.project[projectName]["name"] = projectName;
+    } else {
+      console.log(chalk.red("Project has already existed!"));
+      process.exit();
+    }
+
+    const cmdStr = `git clone ${gitUrl} && mv creditcloudfrontendstage ${projectName}`;
+
+    console.log(chalk.white("\n Start generating..."));
+
+    execPromise(cmdStr).then(() => {
+      const currentDir = shelljs.pwd();
+      const projectDir = `${currentDir}/${projectName}`;
+      // 删除git信息
+      shelljs.rm("-rf", `${projectDir}/.git`);
+      console.log(chalk.green("\n √ Generation completed!"));
+      console.log(`\n cd ${projectName} && npm install || cnpm install \n`)
+    });
+  });
+};
