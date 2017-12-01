54dde47b2c24b34ed0d588832c50d38e89d39789
youngclean <mufeng_606@hotmail.com>
2017-12-01 11:39:07 +0800

    修改判断项目存不存在的逻辑

--- command/init.js
+++ command/init.js
@@ -2,14 +2,14 @@
 const exec = require("child_process").exec;
 const co = require("co");
 const prompt = require("co-prompt");
-const config = require("../config");
 const chalk = require("chalk");
 const shelljs = require("shelljs");
 const fs = require("fs");
 
 const execPromise = (command) => {
   return new Promise((resolve, reject) => {
-    console.log(chalk.white(`Start to exec commands:${command}`));
+    console.log("\r");
+  //  console.log(chalk.white(`Start to exec commands:${command}`));
     const cmd = exec(command, err => {
       if (err) {
         return reject(err);
@@ -43,13 +43,17 @@ module.exports = () => {
       projectName = yield prompt("Project name: ");
     }
 
+    const currentDir = shelljs.pwd();
+
     // 避免重复添加
-    if (!config.project[projectName]) {
-      config.project[projectName] = {};
-      config.project[projectName]["name"] = projectName;
-    } else {
-      console.log(chalk.red("Project has already existed!"));
-      process.exit();
+    if (fs.existsSync(`${currentDir}/${projectName}`)) {
+      console.log(chalk.red(`Project ${projectName} has already existed!`));
+      const checkDel = yield prompt.confirm("Do you want to reInstall it?(y or n): ");
+      if(checkDel){
+        shelljs.rm("-rf", `${currentDir}/${projectName}`);
+      } else {
+        process.exit();
+      }
     }
 
     const checkCnpm = yield prompt.confirm("是否安装了cnpm?(y or n): ");
@@ -58,7 +62,6 @@ module.exports = () => {
     console.log(chalk.blue("Start generating..."));
 
     execPromise(`git clone ${webpackGitUrl}`).then(() => {
-      const currentDir = shelljs.pwd();
       const webpackDir = `${currentDir}/webpack-hot-demo`;
       // 删除webpackDir中的无效文件
       shelljs.rm("-rf", `${webpackDir}/.git`);
@@ -89,12 +92,12 @@ module.exports = () => {
           shelljs.cp(`${webpackDir}/*`, `${projectDir}/`);
           shelljs.rm("-rf", webpackDir);
           if(checkCnpm){
-            console.log(chalk.yellow("Try using cnpm install, please waiting..."));
+            console.log(chalk.yellow("Try to use cnpm install, please waiting..."));
             execPromise(`cd ${projectName} && cnpm install`).then(() => {
               endFun(projectName);
             });
           } else {
-            console.log(chalk.yellow("Try using npm install, please waiting..."));
+            console.log(chalk.yellow("Try to use npm install, please waiting..."));
             execPromise(`cd ${projectName} && npm install`).then(() => {
               endFun(projectName)
             });
@@ -103,12 +106,12 @@ module.exports = () => {
       } else {
         execPromise(`mv webpack-hot-demo ${projectName}`).then(() => {
           if(checkCnpm){
-            console.log(chalk.yellow("try using cnpm install, please waiting..."));
+            console.log(chalk.yellow("Try to use cnpm install, please waiting..."));
             execPromise(`cd ${projectName} && cnpm install`).then(() => {
               endFun(projectName);
             });
           } else {
-            console.log(chalk.yellow("try using npm install, please waiting..."));
+            console.log(chalk.yellow("Try to use npm install, please waiting..."));
             execPromise(`cd ${projectName} && npm install`).then(() => {
               endFun(projectName)
             });
